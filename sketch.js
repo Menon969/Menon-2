81ffe415e892774248835177307d8917650fcf31
Menon969 <menondepaula@gmail.com>
2024-05-07 13:50:33 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600, 600);
+   background("gray");
+  }
+  
+  function draw() {
+  
+   stroke("black")
+   fill("red")
+  
+   // console.log(mouseIsPressed);
+   if(mouseIsPressed) {
+  
+   rect(mouseX, mouseY, 20, 20);
+   }
+  
+  }
